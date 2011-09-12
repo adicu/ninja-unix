807a171af1e9a8e919cb54fc07063fd7d9a51246
Zhehao Mao <zhehao.mao@gmail.com>
2011-09-12 09:30:49 -0400

    make delprinters.sh script standalone

--- delprinters.sh
+++ delprinters.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+function download {
+	URL=$1
+	FILE=`basename $1`
+	if [ -z `which wget` ]; then
+		if [ -z `which curl` ]; then
+			echo "Please install either wget or curl"
+		else
+			curl $URL > $FILE 
+		fi
+	else
+		wget $URL
+	fi
+}
+
 LPADMIN=`which lpadmin`
 
 if [ -z $LPADMIN ]; then
 	echo "Could not find the lpadmin program."
 	echo "Either you have not installed CUPS, or lpadmin is not on your PATH"
+fi
+
+if [ ! -f printers.conf ]; then
+	URL=https://raw.github.com/adicu/ninja-unix/master/printers.conf
+	echo "No printers.conf. Attempting to download."
+	download $URL	
 fi
 
 read_config(){
