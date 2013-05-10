85d30e94d9b14bbb7f225b6107d8fbabfd631392
Nate Brennnand <natebrennand@gmail.com>
2013-05-09 20:52:21 -0400

    fixes and refactoring
    Removed double quotation from output (script could not read).
    Switched from regex to simple string manipulation to allow NWCB to be added (bad formatting broke it).
    Sorts printer output

--- scraper.py
+++ scraper.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
-#Moses Nakamura
-import re
+#Moses Nakamura & Nate Brennand
 import urllib
 
 
 #all of the js files that have the information we need
-links=["http://www.columbia.edu/acis/facilities/printers/ninja/acis/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/barnard/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/cait/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/lso/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/meche/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/old_pcd/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/soa/js/printers.js","http://www.columbia.edu/acis/facilities/printers/ninja/ssw/js/printers.js"]
+links=["http://www.columbia.edu/acis/facilities/printers/ninja/acis/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/barnard/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/cait/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/lso/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/meche/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/old_pcd/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/soa/js/printers.js",
+    "http://www.columbia.edu/acis/facilities/printers/ninja/ssw/js/printers.js"]
 
-searcher = re.compile(r"\[([^\]]*)\];")
 lst=[]
-
 #scrapes the files, puts the information into lists in lists
 for link in links:
     filep = urllib.urlopen(link)
     for line in filep:
-        matched = searcher.search(line)
-        if matched:
-            #note: the strings are surrounded by the character ", 
-            #which is actually pretty useful.  Don't need to change this.
-            lst.append(matched.group(1).split(","))
-
-
+        if line.count('[') == 2:
+            line = line.replace('"', '') # removing double quotations from strings
+            line = line[line.rfind('[')+1 : line.rfind(']')-1]
+            lst.append(line.split(","))
 
 #organizes the information nicely
 towrite = []
 
-for line in lst:
-    towrite.append(line[0] + " " + line[1] + " " + line[3] + "\n")
+for line in sorted(lst):
+    towrite.append("{} {} {}\n".format(line[0], line[1], line[3]))
 
 #writes it into the correct format in newprinter.yaml file
 filep = open("printers.conf","w")
 filep.writelines(towrite)
-
-
 
